5f32245736c2374da9d504eabe65a9d3105bee1e
ian-j <ian.jones@dangermunki.com>
2017-07-06 01:52:35 -0600

    Now calling becomeFirstResponder() in viewDidAppear

--- Doctor's Note/Doctor's Note/NewNoteViewController.swift
+++ Doctor's Note/Doctor's Note/NewNoteViewController.swift
@@ -14,32 +14,30 @@ class NewNoteViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var saveButton: UIButton!
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        newNoteTextView.becomeFirstResponder()
+        
         newNoteTextView.isEditable = true
         
         // Do any additional setup after loading the view.
     }
+    
     override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
         newNoteTextView.becomeFirstResponder()
         newNoteTextView.isEditable = true
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        
+        newNoteTextView.becomeFirstResponder()
     }
-    */
+
+    
+
 
 }
